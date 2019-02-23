28996292130a8aba1c608e88cfa3b58368dbb3f5
mohamadmowfakhlal <mohamadmowfakhlal@gmail.com>
2019-02-23 16:23:59 +0100

    modified exercise for the second and third lessons

--- exercise.sql
+++ exercise.sql
@@ -1,28 +1,43 @@
-elect * from Section where Building='Taylor';
-#select * from Department;
-#update Department set Building='Taylor' where DeptName='Finance' ;
-#lect * from Takes where CourseIDa='CS-102' ;
-#select c.CourseID,t.StudyYear,t.Grade from Course c natural join  Takes t natural join Student s where s.StudName='Shankar';
-#insert Course values('CS-103','Monthly Seminar','Comp. Sci.',0);
-#update Section Set StudyYear='2010' where CourseID='CS-102';
-#Select * from Student where Student.DeptName='Comp. Sci.' 
-#select * from Section where CourseID='CS-102'
-#insert into Takes (StudID,SectionID,CourseID,Semester,StudyYear)
- #Select StudID,'1','CS-102','Fall','2010' from Student where Student.DeptName='Comp. Sci.';
- #delete from  Takes where CourseID='CS-103'
- #use University;
-#select * from Takes;
-#select STudyYear,CourseID,Count(StudID) from Takes Where StudyYear=2009 Group By CourseID ; 
-#select sum(C.Credits),C.Title from Department D join Instructor I join Course C where I.InstName='Brandt' group by C.Title
+
 use University;
 #select courseID from Section;
 #delete  from Course  where CourseID ='sadfsafsa';
 #delete  from Course  where CourseID not in(
 #select courseID from Section);
+
+#Find the highest salary of any instructor
 #select Max(Salary) from Instructor
+
+#Find all instructors earning the highest salary.
+#There might be more than one with the same
+#salary
 #select InstName,salary from Instructor where salary > (select AVG(salary) from Instructor)
+
+#Delete courses BIO-101 and BIO-301 in the Takes table.
 #Delete from Takes where CourseID in ('BIO-101','BIO-301')
+
+#Find those students who have not taken a course.
 #select StudID,StudName from Student where StudID not in (Select StudID from Takes)
+#Find the names of those departments which have
+#a budget which is higher than those for all other departments
 #select * from Department where Budget > All(select AVG(Budget) from Department);
+
+#Find the names of those students who have the same name as some instructor. Use the SOME  operator for this
 #select * from Student where StudName = some (select InstName from Instructor)
-select * from Student where StudName in (Select InstName from Instructor)
+
+#Delete all courses that have never been offered
+#(that is, do not occur in the Section table
+
+#select * from Student where StudName in (Select InstName from Instructor)
+
+
+#we have two correction
+#note there is a difference between naturlal join and inner join 
+#Find the number of enrolments of each course
+#section that was offered in Fall 2009.
+#select STudyYear,CourseID,SectionID,Count(StudID) from Takes Where StudyYear=2009 Group By SectionID,CourseID ; 
+
+
+#What is the course title and sum of course credits
+#of the courses taught by instructor Brandt?
+#select Title,sum(Credits) from Instructor I Natural join  Teaches Natural join Course  where I.instName='Brandt' group by Title
