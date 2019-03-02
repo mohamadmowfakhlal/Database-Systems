d3ee03077ccf34f510c61b783d239eb1ef5613a6
mohamadmowfakhlal <mohamadmowfakhlal@gmail.com>
2019-03-02 21:14:10 +0100

    exercise for week4

--- exercise.sql
+++ exercise.sql
@@ -1,16 +1,63 @@
 
 use University;
-#create table Testscores(Student VARCHAR(45),Test CHAR,Score int);
+#select count(*) from Student;
+#select count(*) from Department;
+#select count(*) from Student S natural join Department D
+#4-1-1
+#select S.studID,S.StudName,D.Building from Student S natural join Department D
+#4-1-2
+#select * from Teaches;
+#select * from Instructor;
+#select I.instID,I.InstName,count(SectionID) as sections from Instructor I natural left outer join Teaches T group by InstID;
+#select T.instID,sum(SectionID) from Teaches T group by InstID;
+#select instID,InstName,(select count(SectionID) from Teaches T where T.instID=I.instID) from Instructor I;
+#create view SeniorStudents as 
+#select StudName,StudID from Student S where S.TotCredits > 100
+#select * from SeniorStudents
+create view CreditView11ssddd as(
+select T.StudID,sum(C.Credits) as YEEE,T.StudyYear from Takes T natural left outer join Course C group by T.StudID,T.StudyYear)
+#select * from CreditView1;
 
-#insert into Testscores values('Brandt','A',47);
-/*
-insert into Testscores values('Brandt','B',50);
-insert into Testscores(Student,Test) values('Brandt','C');
-insert into Testscores(Student,Test) values('Brandt','D');
-insert into Testscores values('Chavez','A',52);
-insert into Testscores values('Chavez','B',45);
-insert into Testscores values('Chavez','C',53);
-insert into Testscores(Student,Test) values('Chavez','D');
-*/
-#select * from Testscores;
-select Student,avg(score) from Testscores   group by Student having avg(Score) > 49
+
+
+
+
+
+
+
+2-3-2019
+use University;
+#select count(*) from Student;
+#select count(*) from Department;
+#select count(*) from Student S natural join Department D
+#4-1-1
+#select S.studID,S.StudName,D.Building from Student S natural join Department D
+#4-1-2
+#select * from Teaches;
+#select * from Instructor;
+#select I.instID,I.InstName,count(SectionID) as sections from Instructor I natural left outer join Teaches T group by InstID;
+#select T.instID,sum(SectionID) from Teaches T group by InstID;
+#select instID,InstName,(select count(SectionID) from Teaches T where T.instID=I.instID) from Instructor I;
+#create view SeniorStudents as 
+#select StudName,StudID from Student S where S.TotCredits > 100
+#select * from SeniorStudents
+#create view CreditView11ssddd as(
+#select T.StudID,sum(C.Credits) as YEEE,T.StudyYear from Takes T natural left outer join Course C group by T.StudID,T.StudyYear)
+#select * from CreditView1;
+#select DeptName,Count(InstID) from Department natural left outer join Instructor group by DeptName;
+#select * from (Takes natural left outer join Student)  join Course using(CourseID);
+#select * from Student;
+#select studName,Title from (Takes natural join Student)join Course USING(courseID) order by studName;
+#create view SeniorInstructor as 
+#select InstID,InstName,DeptName from Instructor where salary > 80000
+#select * from SeniorInstructor;
+#create user 'Karen'@'localhost' identified by 'SetPassword';
+#create user 'Linda'@'localhost' identified by 'SetPassword1';
+#create user 'Susan'@'localhost' identified by 'SetPassword2';
+#select user from mysql.user r;
+#grant select on University.* TO 'Karen'@'localhost';
+#grant All on University.* TO 'Linda'@'localhost';
+#grant select on University.* TO 'Susan'@'localhost';
+#show grants for 'Susan'@'localhost'
+#drop user 'Linda'@'localhost';
+#select * from mysql.user r;
