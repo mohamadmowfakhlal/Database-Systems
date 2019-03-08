3c1fff0fc7a1dc0cf770e1240f661bf9b1967a87
mohamadmowfakhlal <mohamadmowfakhlal@gmail.com>
2019-03-08 20:31:16 +0100

    add trigger function event

--- exercise.sql
+++ exercise.sql
@@ -61,3 +61,137 @@ use University;
 #show grants for 'Susan'@'localhost'
 #drop user 'Linda'@'localhost';
 #select * from mysql.user r;
+
+
+
+
+
+use University;
+
+
+
+#drop procedure InstBackup2;
+#drop table InstLog;
+/*
+ DELIMITER //
+create function CPRTest(CPRNumber VARCHAR(10)) RETURNS boolean
+BEGIN 
+DECLARE num INT;
+SET num = (((4 * mid(CPRNumber,1,1))+
+(3 * mid(CPRNumber,2,1)) +
+(2 * mid(CPRNumber,3,1)) +
+(7 * mid(CPRNumber,4,1)) + 
+(6 * mid(CPRNumber,5,1))
++(5 * mid(CPRNumber,6,1))
++ (4 * mid(CPRNumber,7,1))
+ + (3 * mid(CPRNumber,8,1)) 
+ + (2 * mid(CPRNumber,9,1))
+ + (1 * mid(CPRNumber,10,1)))%11);
+
+RETURN num;
+END; //
+DELIMITER;
+#drop function CPRTest;
+
+select CPRTest('1005873661');
+
+
+
+
+*/
+#create table get112 like Instructor;
+
+#select * from get112;
+#drop procedure InstBackup;
+
+#call InstBackup13();
+#insert into Instructor values (2345,'Ahma2d','Finance',40000);
+#show triggers in University;
+#call InstBackup();
+#call InstBackup();
+
+#select * from InstLog;
+ 
+
+#create table InstLog(currentDate Date);
+
+ 
+#drop procedure instBackup16;
+
+
+#select * from instOld4;
+DELIMITER $$
+create procedure InstBackup16()
+BEGIN
+create table instOld16 like InstLog;
+insert into instOld16 select * from InstLog;
+Delete from InstLog;
+select * from instOld16;
+END; $$
+DELIMITER
+#drop table if exists InstLog;
+
+
+
+
+use University;
+#create table InstLog (DateofAdd VARCHAR(20));
+#select * from InstOld;
+
+#drop table if exists InstLog;
+DELIMITER $$
+create procedure InstBackup1()
+BEGIN
+DECLARE vSQLSTATE char(5) DEFAULT '00000';
+DECLARE CONTINUE HANDLER FOR SQLEXCEPTION
+BEGIN 
+GET DIAGNOSTICS CONDITION 1
+vSQLSTATE = RETURNED_SQLSTATE;
+END;
+START TRANSACTION;
+delete from InstOld;
+create table InstOld like InstLog;
+insert into InstOld select * from InstLog;
+Delete from InstLog;
+SELECT vSØLSTATE;
+IF vSQLSTATE='00000' then COMMIT;
+	else ROLLBACK;
+    END IF;
+    
+END; $$
+DELIMITER
+
+
+
+use University;
+#create table InstLog (DateofAdd VARCHAR(20));
+#select * from InstOld;
+
+#insert Instructor values(43345,'amar','Finance',444);
+#select * from InstLog;
+#Set SQL_SAFE_UPDATES=0;
+#Call InstBackup1;
+#select * from InstOld;
+#call InstBackup1;
+
+create table InstLog like Instructor;
+alter table InstLog Add logTime TIMESTAMP(6);
+create EVENT sfs on schedule EVERY 1 week starts '2016-02-21 00:00:01' 
+DO call InstBackup1;
+
+SET GLOBAL event_scheduler = 1;
+show variables like 'event_scheduler';
+
+
+
+
+#create table DiceRolls (RollNo int,DiceEyes int);
+
+#alter table DiceRolls Modify Column	RollNo Int  auto_Increment primary key;
+
+
+
+#drop event dice;
+#select * from DiceRolls where DiceEyes=6;
+#select Distinct DiceEyes,count(*) from DiceRolls where DiceEyes=6  and RollNo < 20 ;
+
